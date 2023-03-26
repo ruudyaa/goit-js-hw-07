e391a4154266d0106b5aa3621f2e710094a4eae7
ruudyaa <martarudnytska26@gmail.com>
2023-03-26 21:40:06 +0300

    Adds code for gallery browsing.

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const imageElement = galleryItems
+  .map(function ({ preview, original, description }) {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+  })
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", imageElement);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
